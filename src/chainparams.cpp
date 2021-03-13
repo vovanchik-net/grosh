86872a00ed9441d3c01b8445a4416563b42688f1
vovanchik.net <coin@vovanchik.net>
2021-03-13 11:19:45 +0300

    0.17.2.3

---
+++ src/chainparams.cpp
@@ -0,0 +1,336 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2018 The Bitcoin Core developers
+// Copyright (c) 2021 Uladzimir(https://t.me/vovanchik_net)
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <chainparams.h>
+#include <consensus/merkle.h>
+
+#include <tinyformat.h>
+#include <util.h>
+#include <utilstrencodings.h>
+#include <uint256.h>
+#include <arith_uint256.h>
+
+#include <assert.h>
+
+#include <chainparamsseeds.h>
+
+static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward) {
+    CMutableTransaction txNew;
+    txNew.nVersion = 1;
+    txNew.vin.resize(1);
+    txNew.vout.resize(1);
+    txNew.vin[0].scriptSig = CScript() << 486604799 << CScriptNum(4) <<
+        ParseHex("54616c6572207065727368616a612062656c617275736b616a61206b727970746176616c697574612062792044656e6973204c2069205365726765204c20");
+    txNew.vout[0].nValue = genesisReward;
+    txNew.vout[0].scriptPubKey
+        << ParseHex("04f360606cf909ce34d4276ce40a5dd6a844a4a72473086e0fc635f3c4195d77df513b7541dc5f6f6d01ec39e4b729893c6d42dd5e248379a32b5259f38f6bfbae") 
+        << OP_CHECKSIG;
+
+    CBlock genesis;
+    genesis.nTime    = nTime;
+    genesis.nBits    = nBits;
+    genesis.nNonce   = nNonce;
+    genesis.nVersion = nVersion;
+    genesis.vtx.push_back(MakeTransactionRef(std::move(txNew)));
+    genesis.hashPrevBlock.SetNull();
+    genesis.hashMerkleRoot = BlockMerkleRoot(genesis);
+    return genesis;
+}
+
+void CChainParams::UpdateVersionBitsParameters(Consensus::DeploymentPos d, int64_t nStartTime, int64_t nTimeout)
+{
+    consensus.vDeployments[d].nStartTime = nStartTime;
+    consensus.vDeployments[d].nTimeout = nTimeout;
+}
+
+/**
+ * Main network
+ */
+ 
+static SeedSpec6 pnSeed6_mmain[] = {
+    {{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0xff,0xff, 185, 231,  70,  34}, 12358},
+    {{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0xff,0xff,  86,  57, 195, 202}, 12358},
+};
+
+class CMainParams : public CChainParams {
+public:
+    CMainParams() {
+        strNetworkID = "main";
+        consensus.nSubsidyHalvingInterval = 210000 * 5;
+        consensus.BIP34Height = 0;
+        consensus.BIP34Hash = uint256();
+        consensus.BIP65Height = 32256;
+        consensus.BIP66Height = 32256;
+        consensus.CSVHeight = 32256;
+        consensus.WitnessHeight = 32256;
+
+        consensus.powLimit = (~arith_uint256 (0)) >> 8;
+        consensus.powLimitLegacy = (~arith_uint256 (0)) >> 20;
+        consensus.nPowTargetTimespan = 10 * 60;
+        consensus.nPowTargetSpacingBegin = 5 * 60;
+        consensus.fPowAllowMinDifficultyBlocks = false;
+        consensus.fPowNoRetargeting = false;
+        consensus.nPosTargetTimespan = 1 * 60 * 60;                 // 1 hour
+        consensus.nPosTargetSpacing = 2.5 * 60;
+        consensus.nCoinAgeTick = 60 * 60 * 24;
+        consensus.nStakeMinAge = 60 * 60 * 24 * 2;              // minimum age for coin age  
+        consensus.nStakeMaxAge = 60 * 60 * 24 * 90;             // stake age of full weight
+        consensus.nStakeModifierInterval = 6 * 60 * 60;         // time to elapse before new modifier is computed
+        consensus.posLimit = (~arith_uint256 (0)) >> 24;
+
+        consensus.nLyra2ZHeight = 10000;
+        consensus.nPowAveragingWindowv1 = 24;
+        consensus.TLRHeight = 269371;
+        consensus.nNewDiffAdjustmentAlgorithmHeight = 269371;
+        consensus.nPowAveragingWindowv2 = 120;
+        
+        consensus.newProofHeight = 269371;
+
+        // The best chain should have at least this much work.
+        consensus.nMinimumChainWork =
+            uint256S("0x0000000000000000000000000000000000000000000000000089d259b49d26f8"); // 200000 
+
+        // By default assume that the signatures in ancestors of this block are valid.
+        consensus.defaultAssumeValid =
+            uint256S("0x60c8a6af7ea6cb07818b0a15acb058e4a3ec9f213eee3f2f311483ad0eb1dc31"); // 200000
+
+        /**
+         * The message start string is designed to be unlikely to occur in normal data.
+         * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
+         * a large 32-bit integer with any alignment.
+         */
+        pchMessageStart[0] = 0x64;
+        pchMessageStart[1] = 0xb1;
+        pchMessageStart[2] = 0x73;
+        pchMessageStart[3] = 0xd8;
+        pchNetworkMessageStart[0] = 0x6b;
+        pchNetworkMessageStart[1] = 0x53;
+        pchNetworkMessageStart[2] = 0x41;
+        pchNetworkMessageStart[3] = 0x6d;
+        nDefaultPort = 12358;
+        nPruneAfterHeight = 1000000;
+
+        genesis = CreateGenesisBlock (1505338813, 725170, 0x1e0ffff0, 1, 50 * COIN);
+        consensus.hashGenesisBlock = genesis.GetHash();
+        assert(consensus.hashGenesisBlock ==
+               uint256S("0xc079fd1ae86223e1522928776899d46e329da7919ca1e11be23643c67dd05d5f"));
+        assert(genesis.hashMerkleRoot ==
+               uint256S("0x985fae483ebbef9cde04a259282cb7465d52bf56824caf1a8132395e90488b12"));
+        // Note that of those which support the service bits prefix, most only support a subset of
+        // possible options.
+        // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
+        // service bits we want, but we should get them updated to support all service bits wanted by any
+        // release ASAP to avoid it where possible.
+        vSeeds.clear();
+        vSeeds.emplace_back("seed.vovanchik.net");
+
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 65);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 50);
+        base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1, 193);
+        base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E};
+        base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4};
+
+        bech32_hrp = "tlr";
+
+        vFixedSeeds.clear();
+        vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_mmain, pnSeed6_mmain + ARRAYLEN(pnSeed6_mmain));
+
+        fDefaultConsistencyChecks = false;
+        fRequireStandard = true;
+        fMineBlocksOnDemand = false;
+
+        checkpointData = {
+            {
+                {2048, uint256S("0xc4838cab89b16915d813f424198a999af82b3dce2afed5d82cab1fe9df08d701")},
+                {20000, uint256S("0x82ad64f451be0375683efbdc7d94c1b970431b02a6a3e5057dd6cd0fb2022e70")},
+                {100000, uint256S("0xa6c3e93e8ac7b4af077a78c6ce27a1b2b8b7793a7737403bcb9e6f420a928547")},
+                {200000, uint256S("0x60c8a6af7ea6cb07818b0a15acb058e4a3ec9f213eee3f2f311483ad0eb1dc31")},
+            }
+        };
+
+        /* disable fallback fee on mainnet */
+        m_fallback_fee_enabled = false;
+    }
+};
+
+/**
+ * Testnet (v3)
+ */
+class CTestNetParams : public CChainParams {
+public:
+    CTestNetParams() {
+        strNetworkID = "test";
+        consensus.nSubsidyHalvingInterval = 20000;
+        consensus.BIP34Height = 0;
+        consensus.BIP34Hash = uint256();
+        consensus.BIP65Height = 5;
+        consensus.BIP66Height = 5;
+        consensus.CSVHeight = 5;
+        consensus.WitnessHeight = 5;
+
+        consensus.powLimit = (~arith_uint256 (0)) >> 16;
+        consensus.powLimitLegacy = (~arith_uint256 (0)) >> 16;
+        consensus.nPowTargetTimespan = 30 * 60;
+        consensus.nPowTargetSpacingBegin = 2.5 * 60;
+        consensus.fPowAllowMinDifficultyBlocks = false;
+        consensus.fPowNoRetargeting = false;
+        consensus.nPosTargetTimespan = 14 * 24 * 60 * 60;       // two weeks
+        consensus.nPosTargetSpacing = 60 * 7 / 3;
+        consensus.nCoinAgeTick = 10 * 60;
+        consensus.nStakeMinAge = 10 * 60 * 2;                   // minimum age for coin age  
+        consensus.nStakeMaxAge = 10 * 60 * 90;                  // stake age of full weight
+        consensus.nStakeModifierInterval = 6 * 60;              // time to elapse before new modifier is computed
+        consensus.posLimit = (~arith_uint256 (0)) >> 24;
+
+        consensus.nLyra2ZHeight = 10;
+        consensus.nPowAveragingWindowv1 = 5;
+        consensus.TLRHeight = 20;
+        consensus.nNewDiffAdjustmentAlgorithmHeight = 21000;
+        consensus.nPowAveragingWindowv2 = 120;
+
+        consensus.newProofHeight = 30;
+
+        // The best chain should have at least this much work.
+        consensus.nMinimumChainWork = uint256S("0x00");
+
+        // By default assume that the signatures in ancestors of this block are valid.
+        consensus.defaultAssumeValid = uint256S("0x00");
+
+        pchMessageStart[0] = 0xfd;
+        pchMessageStart[1] = 0xd2;
+        pchMessageStart[2] = 0xc8;
+        pchMessageStart[3] = 0x07;
+        nDefaultPort = 18333;
+        nPruneAfterHeight = 10000;
+ 
+        genesis = CreateGenesisBlock (1505338823, 61468, 0x1f00ffff, 1, 50 * COIN);
+        consensus.hashGenesisBlock = genesis.GetHash();
+        assert(consensus.hashGenesisBlock ==
+               uint256S("0xf3b7fe4392b75efcaeb9fe5a617912a763572c69d088896bfe3cf796b1a6a866"));
+        assert(genesis.hashMerkleRoot ==
+               uint256S("0x985fae483ebbef9cde04a259282cb7465d52bf56824caf1a8132395e90488b12")); 
+
+        vSeeds.clear();
+        vFixedSeeds.clear();
+
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 111);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 196);
+        base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1, 239);
+        base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x35, 0x87, 0xCF};
+        base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94}; 
+        
+        bech32_hrp = "test";
+
+        fDefaultConsistencyChecks = false;
+        fRequireStandard = false;
+        fMineBlocksOnDemand = false;
+
+        checkpointData = {
+            {
+                {0, uint256S("0xf3b7fe4392b75efcaeb9fe5a617912a763572c69d088896bfe3cf796b1a6a866")},
+            }
+        };
+
+        /* enable fallback fee on testnet */
+        m_fallback_fee_enabled = true;
+    }
+};
+
+/**
+ * Regression test
+ */
+class CRegTestParams : public CChainParams {
+public:
+    CRegTestParams() {
+        strNetworkID = "regtest";
+        consensus.nSubsidyHalvingInterval = 150;
+        consensus.BIP34Height = 100000000; // BIP34 has not activated on regtest (far in the future so block v1 are not rejected in tests)
+        consensus.BIP34Hash = uint256();
+        consensus.BIP65Height = 1351; // BIP65 activated on regtest (Used in rpc activation tests)
+        consensus.BIP66Height = 1251; // BIP66 activated on regtest (Used in rpc activation tests)
+        consensus.CSVHeight = 1400;
+        consensus.WitnessHeight = 1400;
+        consensus.powLimit = (~arith_uint256 (0)) >> 4;
+        consensus.powLimitLegacy = (~arith_uint256 (0)) >> 16;
+        consensus.posLimit = (~arith_uint256 (0)) >> 8;
+        consensus.nPowTargetTimespan = 24 * 60 * 60;
+        consensus.nPowTargetSpacingBegin = 5 * 60;
+        consensus.fPowAllowMinDifficultyBlocks = true;
+        consensus.fPowNoRetargeting = true;
+        consensus.newProofHeight = 30;
+
+        // The best chain should have at least this much work.
+        consensus.nMinimumChainWork = uint256S("0x00");
+
+        // By default assume that the signatures in ancestors of this block are valid.
+        consensus.defaultAssumeValid = uint256S("0x00");
+
+        pchMessageStart[0] = 0xfa;
+        pchMessageStart[1] = 0xbf;
+        pchMessageStart[2] = 0xb5;
+        pchMessageStart[3] = 0xda;
+        nDefaultPort = 18444;
+        nPruneAfterHeight = 1000;
+
+        genesis = CreateGenesisBlock(1296688602, 2, 0x207fffff, 1, 50 * COIN);
+        consensus.hashGenesisBlock = genesis.GetHash();
+        assert(consensus.hashGenesisBlock == uint256S("0x0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206"));
+        assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
+
+        vFixedSeeds.clear(); //!< Regtest mode doesn't have any fixed seeds.
+        vSeeds.clear();      //!< Regtest mode doesn't have any DNS seeds.
+
+        fDefaultConsistencyChecks = true;
+        fRequireStandard = false;
+        fMineBlocksOnDemand = true;
+
+        checkpointData = {
+            {
+                {0, uint256S("0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206")},
+            }
+        };
+
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,239);
+        base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x35, 0x87, 0xCF};
+        base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94};
+
+        bech32_hrp = "bcrt";
+
+        /* enable fallback fee on regtest */
+        m_fallback_fee_enabled = true;
+    }
+};
+
+static std::unique_ptr<CChainParams> globalChainParams;
+
+const CChainParams &Params() {
+    assert(globalChainParams);
+    return *globalChainParams;
+}
+
+std::unique_ptr<CChainParams> CreateChainParams(const std::string& chain)
+{
+    if (chain == CBaseChainParams::MAIN)
+        return std::unique_ptr<CChainParams>(new CMainParams());
+    else if (chain == CBaseChainParams::TESTNET)
+        return std::unique_ptr<CChainParams>(new CTestNetParams());
+    else if (chain == CBaseChainParams::REGTEST)
+        return std::unique_ptr<CChainParams>(new CRegTestParams());
+    throw std::runtime_error(strprintf("%s: Unknown chain %s.", __func__, chain));
+}
+
+void SelectParams(const std::string& network)
+{
+    SelectBaseParams(network);
+    globalChainParams = CreateChainParams(network);
+}
+
+void UpdateVersionBitsParameters(Consensus::DeploymentPos d, int64_t nStartTime, int64_t nTimeout)
+{
+    globalChainParams->UpdateVersionBitsParameters(d, nStartTime, nTimeout);
+}
---
+++ src/pow.cpp
@@ -0,0 +1,213 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2018 The Bitcoin Core developers
+// Copyright (c) 2021 Uladzimir(https://t.me/vovanchik_net)
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <pow.h>
+
+#include <arith_uint256.h>
+#include <chain.h>
+#include <primitives/block.h>
+#include <uint256.h>
+
+//pos: find last block index up to pindex
+const CBlockIndex *GetLastBlockIndex(const CBlockIndex *pindex, const Consensus::Params &params, bool fProofOfStake) {
+    if (fProofOfStake) {
+        while (pindex && pindex->pprev && !pindex->IsProofOfStake()) {
+            if (params.forkNumber(pindex->nHeight) < 2) 
+                return nullptr;
+            pindex = pindex->pprev;
+        }
+    } else {
+        while (pindex && pindex->pprev && pindex->IsProofOfStake())
+            pindex = pindex->pprev;
+    }
+
+    return pindex;
+}
+
+uint32_t
+GetNextWorkRequiredForPos(const CBlockIndex *pindexLast, const CBlockHeader *pblock, const Consensus::Params &params) {
+    assert(params.forkNumber(pindexLast->nHeight + 1) >= 2);
+
+    const CBlockIndex *pindexPrev = GetLastBlockIndex(pindexLast, params, true);
+    if (pindexPrev == nullptr)
+        return params.posLimit.GetCompact();
+
+    const CBlockIndex *pindexPrevPrev = GetLastBlockIndex(pindexPrev->pprev, params, true);
+    if (pindexPrevPrev == nullptr || pindexPrevPrev->pprev == nullptr)
+        return params.posLimit.GetCompact();
+
+    const int64_t nActualSpacing = pindexPrev->GetBlockTime() - pindexPrevPrev->GetBlockTime();
+    const int64_t nInterval = params.DifficultyAdjustmentIntervalPos();
+    arith_uint256 bnNew;
+    bnNew.SetCompact(pindexPrev->nBits);
+    bnNew *= ((nInterval - 1) * params.nPosTargetSpacing + nActualSpacing + nActualSpacing);
+    //overflow fix
+    if((pindexLast->nHeight + 1)>=730000)
+    {
+        arith_uint256 powLimit = UintToArith256(uint256S("dfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"));
+        if (bnNew > powLimit)
+            bnNew = powLimit;
+    }
+    //    
+    bnNew /= ((nInterval + 1) * params.nPosTargetSpacing);
+    if (bnNew > params.powLimit) bnNew = params.powLimit;
+    return bnNew.GetCompact();
+}
+
+unsigned int
+GetNextWorkRequiredBTC(const CBlockIndex *pindexLast, const CBlockHeader *pblock, const Consensus::Params &params) {
+    assert(pindexLast != nullptr);
+    unsigned int nProofOfWorkLimit = params.powLimitLegacy.GetCompact();
+
+    // Only change once per difficulty adjustment interval
+    if ((pindexLast->nHeight + 1) % params.DifficultyAdjustmentIntervalPow(pindexLast->nHeight + 1) != 0) {
+        if (params.fPowAllowMinDifficultyBlocks) {
+            // Special difficulty rule for testnet:
+            // If the new block's timestamp is more than 2* 10 minutes
+            // then allow mining of a min-difficulty block.
+            if (pblock->GetBlockTime() > pindexLast->GetBlockTime() +
+                                         params.nPowTargetSpacing(pindexLast->nHeight) * 2)
+                return nProofOfWorkLimit;
+            else {
+                // Return the last non-special-min-difficulty-rules-block
+                const CBlockIndex *pindex = pindexLast;
+                while (pindex->pprev &&
+                       pindex->nHeight % params.DifficultyAdjustmentIntervalPow(pindex->nHeight) != 0 &&
+                       pindex->nBits == nProofOfWorkLimit)
+                    pindex = pindex->pprev;
+                return pindex->nBits;
+            }
+        }
+        return pindexLast->nBits;
+    }
+
+    int64_t blockstogoback = params.DifficultyAdjustmentIntervalPow(pindexLast->nHeight + 1) - 1;
+    if ((pindexLast->nHeight + 1) != params.DifficultyAdjustmentIntervalPow(pindexLast->nHeight + 1))
+        blockstogoback = params.DifficultyAdjustmentIntervalPow(pindexLast->nHeight + 1);
+
+    // Go back by what we want to be 14 days worth of blocks
+    const CBlockIndex *pindexFirst = pindexLast;
+    for (int i = 0; pindexFirst && i < blockstogoback; i++)
+        pindexFirst = pindexFirst->pprev;
+
+    assert(pindexFirst);
+
+    if (params.fPowNoRetargeting) return pindexLast->nBits;
+    // Limit adjustment step
+    int64_t nActualTimespan = pindexLast->GetBlockTime() - pindexFirst->GetBlockTime();
+    if (nActualTimespan < params.nPowTargetTimespan / 4) nActualTimespan = params.nPowTargetTimespan / 4;
+    if (nActualTimespan > params.nPowTargetTimespan * 4) nActualTimespan = params.nPowTargetTimespan * 4;
+    // Retarget
+    arith_uint256 bnNew;
+    bnNew.SetCompact(pindexLast->nBits);
+    bool fShift = bnNew.bits() > params.powLimitLegacy.bits() - 1;
+    if (fShift) bnNew >>= 1;
+    bnNew *= nActualTimespan;
+    bnNew /= params.nPowTargetTimespan;
+    if (fShift) bnNew <<= 1;
+    if (bnNew > params.powLimitLegacy) bnNew = params.powLimitLegacy;
+    return bnNew.GetCompact();
+}
+
+unsigned int DarkGravityWaveOld(const CBlockIndex *pindexLast, const Consensus::Params &params) {
+    const CBlockIndex* pLast = pindexLast;
+    int64_t nActualTimespan = 0;
+    int64_t nBlocks = params.nPowAveragingWindowv1;
+    arith_uint256 bnNew;
+    if (pLast == nullptr || pLast->nHeight < nBlocks) return params.powLimit.GetCompact();
+    for (int i = 0; i < nBlocks; i++) {
+        if (i == 0) bnNew = arith_uint256().SetCompact(pLast->nBits);
+        bnNew += arith_uint256().SetCompact(pLast->nBits);
+        const CBlockIndex* pCur = pLast->pprev;            
+        if (i < nBlocks-1) nActualTimespan += (int64_t) pLast->GetBlockTime() - pCur->GetBlockTime();
+        pLast = pCur;
+    }
+    bnNew /= (nBlocks+1);
+    bnNew -= nBlocks+1;
+    int64_t nTargetTimespan = nBlocks * params.nPowTargetSpacing (pindexLast->nHeight);
+    if (nActualTimespan < nTargetTimespan / 3) nActualTimespan = nTargetTimespan / 3;
+    if (nActualTimespan > nTargetTimespan * 3) nActualTimespan = nTargetTimespan * 3;
+    // Retarget
+    bnNew *= nActualTimespan;
+    bnNew /= nTargetTimespan;
+    if (bnNew > params.powLimit) bnNew = params.powLimit;
+    return bnNew.GetCompact();
+}
+
+unsigned int DarkGravityWave(const CBlockIndex *pindexLast, const Consensus::Params &params) {
+    const CBlockIndex* pLast = GetLastBlockIndex(pindexLast, params, false);
+    int64_t nActualTimespan = 0;
+    int64_t nBlocks = params.nPowAveragingWindowv2;
+    arith_uint256 bnNew;
+    if (pLast == nullptr || pLast->nHeight < nBlocks) return params.powLimit.GetCompact();
+    for (int i = 0; i < nBlocks; i++) {
+        if (i == 0) bnNew = arith_uint256().SetCompact(pLast->nBits);
+        bnNew += arith_uint256().SetCompact(pLast->nBits);
+        const CBlockIndex* pCur = GetLastBlockIndex(pLast->pprev, params, false);            
+        if (i < nBlocks-1) nActualTimespan += (int64_t) pLast->GetBlockTime() - pCur->GetBlockTime();
+        pLast = pCur;
+    }
+    bnNew /= (nBlocks+1);
+    bnNew -= nBlocks+1;
+    int64_t nTargetTimespan = nBlocks * params.nPowTargetSpacing (pindexLast->nHeight);
+    if (nActualTimespan < nTargetTimespan / 3) nActualTimespan = nTargetTimespan / 3;
+    if (nActualTimespan > nTargetTimespan * 3) nActualTimespan = nTargetTimespan * 3;
+    // Retarget
+    bnNew *= nActualTimespan;
+    bnNew /= nTargetTimespan;
+    if (bnNew > params.powLimit) bnNew = params.powLimit;
+    return bnNew.GetCompact();
+}
+
+unsigned int
+GetNextWorkRequiredForPow(const CBlockIndex *pindexLast, const CBlockHeader *pblock, const Consensus::Params &params) {
+    int nNextHeight = pindexLast->nHeight + 1;
+
+    if (nNextHeight < params.nLyra2ZHeight) {
+        return GetNextWorkRequiredBTC(pindexLast, pblock, params);
+    } else if (nNextHeight < params.nLyra2ZHeight + params.nPowAveragingWindowv1) {
+        return params.powLimit.GetCompact();
+    } else if (nNextHeight < params.nNewDiffAdjustmentAlgorithmHeight) {
+        return DarkGravityWaveOld(pindexLast, params);
+    } else if (nNextHeight < params.nNewDiffAdjustmentAlgorithmHeight + params.nPowAveragingWindowv2) {
+        return 0x1c08b5b1;  // 29.39
+    }
+    return DarkGravityWave(pindexLast, params);
+}
+
+uint32_t GetNextWorkRequired(const CBlockIndex *pindexLast, const CBlockHeader *pblock, const Consensus::Params &params) {
+    assert(pindexLast != nullptr);
+    bool fProofOfStake = pblock->IsProofOfStake();
+    if (params.forkNumber(pindexLast->nHeight + 1) < 3)
+        return fProofOfStake ? GetNextWorkRequiredForPos(pindexLast, pblock, params) :
+                               GetNextWorkRequiredForPow(pindexLast, pblock, params);
+    //v1.7 Calc difficulty PoW & PoS. Copyright by Uladzimir(https://t.me/vovanchik_net)
+    arith_uint256 bnOld, bnNew, bnLimit = fProofOfStake ? params.posLimit : params.powLimitLegacy;
+    const CBlockIndex* pPrev = pindexLast;
+    int64_t srcTimes = 0;
+    int64_t nTimes = 0;
+    int TargetSpacing = params.nPowTargetSpacingBegin / 4;
+    if (!fProofOfStake && (pindexLast->nHeight < params.newProofHeight)) return bnLimit.GetCompact();
+    if (fProofOfStake && (pindexLast->nHeight < params.newProofHeight + 7168)) bnLimit >>= 8;
+    int destBlockCount = 12;
+    for (int i = 0; i < destBlockCount; i++) {
+        while (pPrev && (pPrev->IsProofOfStake() != fProofOfStake)) pPrev = pPrev->pprev;
+        if (pPrev == nullptr) return bnLimit.GetCompact();
+        if (pPrev->pprev == nullptr) return bnLimit.GetCompact();
+        if (i == 0) { bnOld.SetCompact (pPrev->nBits); nTimes = pindexLast->GetBlockTime() - pPrev->GetBlockTime(); }
+        bnNew += arith_uint256().SetCompact(pPrev->nBits) * (destBlockCount - i); 
+        srcTimes += std::max(pPrev->GetBlockTime() - pPrev->pprev->GetBlockTime(), (int64_t)0);
+        pPrev = pPrev->pprev;
+    }
+    bnNew /= 2 * 78 * destBlockCount * TargetSpacing;
+    bnNew *= srcTimes;
+    nTimes /= TargetSpacing;
+    while (nTimes > destBlockCount) { bnNew <<= 1; nTimes--; if (bnNew > bnLimit) break; }
+    if (bnNew > (bnOld << 2)) bnNew = bnOld << 2;
+    if (bnNew < (bnOld >> 3)) bnNew = bnOld >> 3;
+    if (bnNew > bnLimit) bnNew = bnLimit;
+    return bnNew.GetCompact();
+}
